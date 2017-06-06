2377e19aa209ed59e957e816aae510e4329b1e48
Neil Okamoto <neil.okamoto@gmail.com>
2017-06-06 09:07:43 -0700

    load cider-nrepl explicitly in the repl (needed this for leiningen development)

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,6 @@
 {:user
   {:plugins
-   [
-    ;; [cider/cider-nrepl "0.14.0"] ; nrepl auto-loaded by cider now
-    [lein-pprint "1.1.2"]    
+   [[lein-pprint "1.1.2"]    
     [jonase/eastwood "0.2.3"]
     [lein-ancient "0.6.10"]
     [lein-bikeshed "0.4.1"]
@@ -13,4 +11,8 @@
    {:init (do (set! *print-length* 500)
               (set! *print-level* 10))}
    :dependencies
-   [[org.clojure/tools.trace "0.7.9"]]}}
+   [[org.clojure/tools.trace "0.7.9"]]}
+
+ :repl
+ {:plugins
+  [[cider/cider-nrepl "0.14.0"]]}}
