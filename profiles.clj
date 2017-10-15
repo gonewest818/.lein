919527c822ff5b26846e9cd31002c4caff4d0454
Neil Okamoto <neil.okamoto@gmail.com>
2017-10-14 17:21:27 -0700

    update dev plugins

--- profiles.clj
+++ profiles.clj
@@ -1,12 +1,12 @@
 {:user
   {:plugins
-   [[lein-pprint "1.1.2"]    
-    [jonase/eastwood "0.2.3"]
-    [lein-ancient "0.6.10"]
-    [lein-bikeshed "0.4.1"]
-    [lein-kibit "0.1.3" :exclusions [org.clojure/clojure
-                                     org.clojure/tools.cli
-                                     org.clojure/tools.namespace]]]
+   [[lein-pprint "1.1.2"     :exclusions [org.clojure/clojure]]    
+    [jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]
+    [lein-ancient "0.6.12"   :exclusions [org.clojure/clojure]]
+    [lein-bikeshed "0.4.1"   :exclusions [org.clojure/clojure]]
+    [lein-kibit "0.1.5"      :exclusions [org.clojure/clojure
+                                          org.clojure/tools.cli
+                                          org.clojure/tools.namespace]]]
    :repl-options
    {:init (do (set! *print-length* 500)
               (set! *print-level* 10))}
