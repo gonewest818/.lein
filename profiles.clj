29c3c3340af4d897391b77336f7b40372816dafa
Neil Okamoto <neil.okamoto@gmail.com>
2017-11-15 14:36:38 -0800

    update dependencies

--- profiles.clj
+++ profiles.clj
@@ -2,8 +2,8 @@
   {:plugins
    [[lein-pprint "1.1.2"     :exclusions [org.clojure/clojure]]    
     [jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]
-    [lein-ancient "0.6.12"   :exclusions [org.clojure/clojure]]
-    [lein-bikeshed "0.4.1"   :exclusions [org.clojure/clojure]]
+    [lein-ancient "0.6.14"   :exclusions [org.clojure/clojure]]
+    [lein-bikeshed "0.5.0"   :exclusions [org.clojure/clojure]]
     [lein-kibit "0.1.5"      :exclusions [org.clojure/clojure
                                           org.clojure/tools.cli
                                           org.clojure/tools.namespace]]]
@@ -15,4 +15,6 @@
 
  :repl
  {:plugins
-  [[cider/cider-nrepl "0.15.1"]]}}
+  [[cider/cider-nrepl "0.15.1"]]
+  :dependencies
+  [[org.clojure/tools.nrepl "0.2.13"]]}}
