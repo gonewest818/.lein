879614ab6f286a7a8a3835c44b2def73093de3d8
Neil Okamoto <neil.okamoto@gmail.com>
2020-03-18 21:53:22 -0700

    bump versions

--- profiles.clj
+++ profiles.clj
@@ -1,27 +1,22 @@
 {:user
   {:plugins
-   [[lein-pprint "1.2.0"     :exclusions [org.clojure/clojure]]
-    [jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]
-    [lein-ancient "0.6.15"   :exclusions [org.clojure/clojure]]
-    [lein-bikeshed "0.5.1"   :exclusions [org.clojure/clojure]]
-    [lein-kibit "0.1.6"      :exclusions [org.clojure/clojure
-                                          org.clojure/tools.cli
-                                          org.clojure/tools.namespace]]
-    [lein-sysutils "0.2.0"]]
+   [[lein-pprint "1.3.2"      :exclusions [org.clojure/clojure]]
+    [jonase/eastwood "0.3.10" :exclusions [org.clojure/clojure]]
+    [lein-ancient "0.6.15"    :exclusions [org.clojure/clojure]]
+    [lein-bikeshed "0.5.2"    :exclusions [org.clojure/clojure]]
+    [lein-kibit "0.1.8"       :exclusions [org.clojure/clojure
+                                           org.clojure/tools.cli
+                                           org.clojure/tools.namespace]]]
 
    :repl-options
    {:init (do (set! *print-length* 500)
               (set! *print-level* 10))}
 
    :dependencies
-   [[org.clojure/tools.trace "0.7.9"]
-    [criterium "0.4.4"]]}
+   [[org.clojure/tools.trace "0.7.10"]
+    [hashp "0.1.1"]
+    [criterium "0.4.5"]]
 
-;;; note: CIDER injects its own dependencies these days
-;; :repl
-;; {:plugins
-;;  [[cider/cider-nrepl "0.17.0-SNAPSHOT"]]
-;;  :dependencies
-;;  [[org.clojure/tools.nrepl "0.2.13"]]}
+   :injections
+   [(require 'hashp.core)]}}
 
-}
